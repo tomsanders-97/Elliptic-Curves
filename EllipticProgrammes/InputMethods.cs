1c12d1b0246391e32f0b5b0bed0e0f4d586ef67e
Thomas Sanders <58152776+tomsanders-97@users.noreply.github.com>
2019-12-12 22:30:22 +0000

    Add files via upload
    Added comments and clearer outputs to aid usability.

---
+++ EllipticProgrammes/InputMethods.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EllipticProgrammes
+{
+    class InputMethods
+    {
+        public static void ProductChoice(int a2, int a4, int a6)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            int productChoice;
+            Console.WriteLine("Please select which program you would like to use.");
+            Console.WriteLine("For SumCalculator, enter {0}", 1);
+            Console.WriteLine("For MultipleCalculator, enter {0}", 2);
+            Console.WriteLine("For TorsionCalculator, enter {0}", 3);
+            productChoice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you.");
+
+            do
+            {
+                if (productChoice == 1)
+                {
+                    EllipticProgrammes.SumCalculator.Method(a2, a4, a6);
+                }
+                else if (productChoice == 2)
+                {
+                    EllipticProgrammes.MultipleCalculator.Method(a2, a4, a6);
+                }
+                else if (productChoice == 3)
+                {
+                    EllipticProgrammes.TorsionCalculator.Method(a2, a4, a6);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product choice; please try again.");
+                }
+            }
+            while (productChoice != 1 && productChoice != 2 && productChoice != 3);
+        }
+
+
+
+
+
+
+
+        public static bool NewProductChoice(int a2, int a4, int a6)
+        {
+            int newProductChoice;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("If you would like to use another product with the same curve, press {0}", 1);
+                Console.WriteLine("If you would like to use another produce with a different curve, press {0}", 2);
+                Console.WriteLine("To exit the program, press {0}", 3);
+                newProductChoice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Thank you.");
+
+                if (newProductChoice == 1)
+                {
+                    EllipticProgrammes.InputMethods.ProductChoice(a2, a4, a6);
+                    return false;
+                }
+                else if (newProductChoice == 2)
+                {
+                    EllipticProgrammes.Program.Main();
+                    return false;
+                }
+                else if (newProductChoice == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product choice; please try again.");
+                    return false;
+                }
+            }
+            while (newProductChoice != 1 && newProductChoice != 2 && newProductChoice != 3);
+        }
+    }
+}
---
+++ EllipticProgrammes/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace EllipticProgrammes
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("This programme is used to perform several operations on elliptic curves.");
+            Console.WriteLine("The Elliptic curve will be defined in the following form:");
+            Console.WriteLine("y^2 = x^3 + a2 x^2 + a4 x + a6.");
+
+            // Input of curve equation, and checking its validity.
+            int a2; int a4; int a6;
+            int ellipticCurveCheck;
+            do
+            {
+                Console.WriteLine("Please input elliptic coeffieients");
+                Console.WriteLine("a2:");
+                a2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("a4:");
+                a4 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("a6:");
+                a6 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Thank you.");
+                ellipticCurveCheck = -4 * a2 * a2 * a2 * a6 + a2 * a2 * a4 * a4 + 18 * a2 * a4 * a6 - 4 * a4 * a4 * a4 - 27 * a6 * a6;
+
+                // Code to eliminate null terms in the equation                     (Could be shortened I'm sure).
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("------------------------------------");
+                if (a4 == 0 && a6 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x^2", a2);
+                }
+                else if (a2 == 0 && a6 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x", a4);
+                }
+                else if (a2 == 0 && a4 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}", a6);
+                }
+                else if (a2 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x + {1}", a4, a6);
+                }
+                else if (a4 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x^2 + {1}", a2, a6);
+                }
+                else if (a6 == 0)
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x^2 + {1}x", a2, a4);
+                }
+                else
+                {
+                    Console.WriteLine("The elliptic curve equation is:");
+                    Console.WriteLine("y^2 = x^3 + {0}x^2 + {1}x + {2}", a2, a4, a6);
+                }
+
+                if (ellipticCurveCheck == 0)
+                {
+                    Console.WriteLine("Invalid coefficient choice; please try again.");
+                }
+            }
+            while (ellipticCurveCheck == 0);
+            Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            // Seleciting a new product, or exiting.
+            bool endCondition;
+            EllipticProgrammes.InputMethods.ProductChoice(a2, a4, a6);
+            do
+            {
+                endCondition = EllipticProgrammes.InputMethods.NewProductChoice(a2, a4, a6);
+            }
+            while (endCondition == false);
+        }
+    }
+}
